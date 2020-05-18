df08e8cf8dc84fed71c51f904f9d9f8c118dcd4b
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2020-05-18 22:18:36 +0200

    Update test stubs

--- pkg/tviso/repository/mongodb.go
+++ pkg/tviso/repository/mongodb.go
@@ -17,7 +17,6 @@ const (
 	database   = "collections"
 	collection = "tviso"
 )
-
 type MongoDB struct {
 	client *mongo.Client
 	encoder *bson.Encoder
@@ -46,7 +45,7 @@ func NewMongoDBRepository() tviso.WriteRepository {
 }
 
 func (m MongoDB) StoreCollection(media []tviso.Media) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	for _, md := range media {
